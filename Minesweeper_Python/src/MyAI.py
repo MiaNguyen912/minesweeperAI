65115f007030878c5681316d0ed3097a7ccb2747
MiaNguyen912 <thuydn7@uci.edu>
2024-05-04 19:45:55 -0700

    update MyAI

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -26,12 +26,13 @@ class MyAI(AI):
 
         # NOTE: the first tile is uncovered automatically for user
         # NOTE: X and Y coordinates of World start at 1, not 0
+        # NOTE: startX and starY are coordinates starting from 0
 
         self.__rowDimension = rowDimension
         self.__colDimension = colDimension
         self.__totalMines = totalMines
-        self.__startX = (startX - 1)  # the inital tile at (__startX, __startY) is guaranteed to be safe (0 mines around)
-        self.__startY = startY - 1
+        self.__startX = startX   # the inital tile at (__startX, __startY) is guaranteed to be safe (0 mines around)
+        self.__startY = startY
         self.__flagRemaining = totalMines
         self.__tilesCovered = ( rowDimension * colDimension - 1 )  # number of covered tiles (including flagged tiles)
         self.__tilesRemaining = ( rowDimension * colDimension - 1 )  # number of covered tiles (not including flagged tiles)
@@ -51,11 +52,9 @@ class MyAI(AI):
         # 							[-1, -1, -1, -1, -1],
         # 							[-1, -1, -1, -1, -1],
         # 						  ]
-        self.__effectiveLabelArray = [
-            [-1 for _ in range(colDimension)] for _ in range(rowDimension)
-        ]
+        self.__effectiveLabelArray = [[-1 for _ in range(colDimension)] for _ in range(rowDimension)]
         self.__effectiveLabelArray[startX][startY] = 0
-        # print(self.__effectiveLabelArray)
+        print(self.__effectiveLabelArray)
 
     def getAction(self, number: int) -> "Action Object":
         pass
@@ -70,46 +69,28 @@ class MyAI(AI):
         # action = AI.Action.UNCOVER
 
         # if done solving the world, leave
-        
-
-
-
         if self.__tilesCovered == self.__totalMines:
             return Action(AI.Action.LEAVE)
 
         # if there are tiles waiting to be uncovered, uncover them
+        print("frontier: ", self.__frontier)
+        print("tiles to uncover: ", self.__tilesToUncover)
         if len(self.__tilesToUncover) > 0:
             x, y = self.__tilesToUncover.pop()
             return Action(AI.Action.UNCOVER, x, y)
 
         # scan the frontier
-        while len(self.__frontier) > 0:
+        if len(self.__frontier) > 0:
             processedTile = self.__frontier.pop()
-            print(processedTile)
             processedTile_X, processedTile_Y = processedTile
+            x_inArray, y_inArray = processedTile_X, processedTile_Y
 
-            if self.__effectiveLabelArray[processedTile_X][processedTile_Y] == 0:
-                for i in range(processedTile_X - 1, processedTile_X + 1 + 1):
-                    for j in range(processedTile_Y - 1, processedTile_Y + 1 + 1):
+            if self.__effectiveLabelArray[x_inArray][y_inArray] == 0:
+                for i in range(x_inArray - 1, min(x_inArray + 1 + 1, self.__rowDimension)):
+                    for j in range(y_inArray - 1, min(y_inArray + 1 + 1, self.__colDimension)):
                         if self.__effectiveLabelArray[i][j] == -1:
                             self.__tilesToUncover.add((i, j))
+                            
+        nextTile_x, nextTile_y = self.__tilesToUncover.pop()
+        return Action(AI.Action.UNCOVER, nextTile_x, nextTile_y)
 
-        # for i in range(self.__rowDimension):
-        #     for j in range(self.__rowDimension):
-        #         if self.__effectiveLabelArray[i][j] == 0: # if the tile has no bomb around
-        #                               zero_locations.append((i, j))
-
-        # while self.__moveCount < 5:
-        #     action = AI.Action(random.randrange(1, len(AI.Action)))
-        #     x = random.randrange(self.__colDimension)
-        #     y = random.randrange(self.__rowDimension)
-        #     self.__moveCount += 1
-        #     return Action(action, x, y)
-
-        # action = AI.Action(random.randrange(len(AI.Action)))
-        # x = random.randrange(self.__colDimension)
-        # y = random.randrange(self.__rowDimension)
-
-        # return Action(action, x, y)
-
-        # return Action(AI.Action.LEAVE)
